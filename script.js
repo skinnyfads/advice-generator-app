1635f830e92812f0cdd6f8a53f05c15515794d16
skinnyfads <skinnyfads@proton.me>
2023-01-10 09:28:48 +0700

    feat(layout): basic style

--- script.js
+++ script.js
@@ -24,7 +24,17 @@ getRandomAdvice.lastId = undefined;
 function setAdvice(number, text) {
   const adviceNumberElement = document.getElementById("advice-number");
   const adviceTextElement = document.getElementById("advice-text");
+  const adviceDelimiterElement = document.getElementById("advice-delimiter");
 
   adviceNumberElement.innerHTML = `<span>Advice #${number}</span>`;
   adviceTextElement.innerHTML = `<span>${text}</span>`;
+  adviceDelimiterElement.innerHTML = `
+    <hr />
+    <svg width="40px" height="40px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        clip-rule="evenodd"
+        d="m10.75 9c0-.41421-.3358-.75-.75-.75-.41421 0-.75.33579-.75.75v6c0 .4142.33579.75.75.75.4142 0 .75-.3358.75-.75zm4 0c0-.41421-.3358-.75-.75-.75s-.75.33579-.75.75v6c0 .4142.3358.75.75.75s.75-.3358.75-.75z"
+      />
+    </svg>
+    <hr />`;
 }
