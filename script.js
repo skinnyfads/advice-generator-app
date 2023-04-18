9f8c7c94fea75f6f8f7405365c21a1ed0a3a08ca
skinnyfads <skinnyfads@proton.me>
2023-04-19 08:25:19 +0900

    feat: create add new advice section

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 window.onload = async () => {
+  changeState(STATE.DISPLAY_QUOTE);
   await displayNewAdvice();
 };
-
+window.onkeyup = (evt) => {
+  if (evt.key === "a") {
+    changeState(STATE.ADD_QUOTE);
+  }
+};
+const STATE = {
+  DISPLAY_QUOTE: 1,
+  ADD_QUOTE: 2,
+};
 document.getElementById("advice-random-dice-button").onclick = async () => {
   await displayNewAdvice();
 };
+
+function changeState(newState) {
+  if (newState === STATE.DISPLAY_QUOTE) {
+    document.querySelector("#advice-add-quote").style.display = "none";
+    document.querySelector("#advice-display-quote").style.display = "flex";
+  } else if (newState === STATE.ADD_QUOTE) {
+    document.querySelector("#advice-add-quote").style.display = "flex";
+    document.querySelector("#advice-display-quote").style.display = "none";
+  }
+}
 
 async function displayNewAdvice() {
   if (getRandomAdvice.running) return;
@@ -14,7 +33,9 @@ async function displayNewAdvice() {
 
 async function getRandomAdvice() {
   setLoading();
-  const response = await fetch("https://ill-gray-centipede-wear.cyclic.app/advices");
+  const response = await fetch(
+    "https://ill-gray-centipede-wear.cyclic.app/advices"
+  );
   const advice = await response.json();
   const adviceId = advice.id;
 
@@ -57,4 +78,19 @@ function setAdvice(number, text) {
       />
     </svg>
     <hr />`;
+}
+
+// textarea script https://stackoverflow.com/a/25621277
+const tx = document.getElementsByTagName("textarea");
+
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute(
+    "style",
+    "height:" + tx[i].scrollHeight + "px;overflow-y:hidden;"
+  );
+  tx[i].addEventListener("input", OnInput, false);
+}
+function OnInput() {
+  this.style.height = 0;
+  this.style.height = this.scrollHeight + "px";
 }
